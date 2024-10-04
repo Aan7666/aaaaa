2b928d42cd0fce1261f8a37436f8c99595695e59
Aan7666 <aannaruto191@gmail.com>
2024-10-05 01:32:38 +0700

    Add files via upload

---
+++ imagine.py
@@ -0,0 +1,27 @@
+"""
+Menyeberangi Jembatan Gantung
+
+Setelah membantu penduduk dengan makanan, mereka mengizinkanmu melintasi jembatan gantung. 
+Namun, jembatan ini sangat rapuh. Hanya orang-orang dengan berat badan tertentu yang boleh menyeberang. 
+Kamu harus menulis program untuk menentukan apakah kamu bisa menyeberang jembatan.
+
+Contoh 1
+Input:
+weight = 50
+
+Output:
+Gak bole
+
+Contoh 2
+Input:
+weight = 90
+
+Output:
+Kamu boleh menyeberang jembatan
+"""
+weight = 90
+# lanjutkan code dibawah ini
+if weight >= 90:
+    print('Kamu boleh menyebrang jembatan')
+else:
+    print('Gak bole')
